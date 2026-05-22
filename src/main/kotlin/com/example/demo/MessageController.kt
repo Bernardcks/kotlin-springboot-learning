788380a77dd37d6df6cc5498577901ac75bf5973
Bernard Chua <bernardchua.2024@computing.smu.edu.sg>
2026-05-22 21:01:39 +0800

    feat: added get Message by id endpoint with http test

--- src/main/kotlin/com/example/demo/MessageController.kt
+++ src/main/kotlin/com/example/demo/MessageController.kt
@@ -2,6 +2,7 @@ package com.example.demo
 
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -19,4 +20,11 @@ class MessageController(private val service: MessageService){
         val savedMessage = service.save(message)
         return ResponseEntity.created(URI("/${savedMessage.id}")).body(savedMessage)
     }
+
+    @GetMapping("/{id}")
+    fun getMessage(@PathVariable id: String): ResponseEntity<Message> =
+        service.findMessageById(id).toResponseEntity()
+
+    private fun Message?.toResponseEntity(): ResponseEntity<Message> =
+        this?.let { ResponseEntity.ok(it) } ?: ResponseEntity.notFound().build()
 }
--- src/main/kotlin/com/example/demo/MessageService.kt
+++ src/main/kotlin/com/example/demo/MessageService.kt
@@ -2,15 +2,18 @@ package com.example.demo
 
 import org.springframework.stereotype.Service
 import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.core.query
 import java.util.UUID
 
 @Service
 class MessageService(private val db: JdbcTemplate) {
-    fun findMessages(): List<Message> = db.query(
-        "select * from messages"
-        ) { response, _ ->
-            Message(response.getString("id"), response.getString("text"))
-        }
+    fun findMessages(): List<Message> = db.query("select * from messages") { response, _ ->
+        Message(response.getString("id"), response.getString("text"))
+    }
+
+    fun findMessageById(id: String): Message? = db.query("select * from messages where id = ?", id) { response, _ ->
+        Message(response.getString("id"), response.getString("text"))
+    }.singleOrNull()
 
     fun save(message: Message): Message {
         val id = message.id ?: UUID.randomUUID().toString()
